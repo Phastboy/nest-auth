a7ea204de0fe2803c6542a43249f08b26085bddb
Hammed Anuoluwapo Pelumi <stationphast@gmail.com>
2024-07-31 10:07:17 +0100

    cleaning

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,32 +1,7 @@
 import { Injectable } from '@nestjs/common';
-import { CreateAuthDto } from './dto/create-auth.dto';
-import { UpdateAuthDto } from './dto/update-auth.dto';
 import { UsersService } from 'src/users/users.service';
-import { CreateUserDto } from 'src/users/dto/create-user.dto';
 
 @Injectable()
 export class AuthService {
-  constructor(
-    private usersService: UsersService,
-    private register: CreateUserDto,
-  ) {}
-  create(createAuthDto: CreateAuthDto) {
-    return this.usersService.create(this.register);
-  }
-
-  findAll() {
-    return this.usersService.findAll();
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} auth`;
-  }
-
-  update(id: number, updateAuthDto: UpdateAuthDto) {
-    return `This action updates a #${id} auth`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} auth`;
-  }
+  constructor(private usersService: UsersService) {}
 }
