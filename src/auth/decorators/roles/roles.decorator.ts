02c727b3c618e938a5aa73c1b4cf850197aaf16e
Hammed Anuoluwapo Pelumi <stationphast@gmail.com>
2025-03-13 01:22:38 +0100

    reset done

--- src/auth/decorators/roles/roles.decorator.ts
+++
@@ -1,8 +0,0 @@
-import { SetMetadata } from '@nestjs/common';
-
-/**
- * Roles Decorator
- * Sets the roles metadata for a route.
- * @param roles - The roles required to access the route.
- */
-export const Roles = (...roles: string[]) => SetMetadata('roles', roles);
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,21 +1,34 @@
-import { Controller, Get, Param, Patch, Body, UseGuards } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
 import { UsersService } from './users.service';
-import { JwtAuthGuard } from '../auth/gaurds/jwt-auth/jwt-auth.guard';
-import { Roles } from '../auth/decorators/roles/roles.decorator';
-import { RolesGuard } from '../auth/gaurds/role/role.guard';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
 
-/**
- * Users Controller
- * Handles requests related to user operations.
- */
 @Controller('users')
-@UseGuards(JwtAuthGuard, RolesGuard)
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
-  @Get(':username')
-  @Roles('admin')
-  async findOne(@Param('username') username: string) {
-    return this.usersService.findByUsername(username);
+  @Post()
+  create(@Body() createUserDto: CreateUserDto) {
+    return this.usersService.create(createUserDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.usersService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.usersService.findOne(+id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+    return this.usersService.update(+id, updateUserDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.usersService.remove(+id);
   }
 }
