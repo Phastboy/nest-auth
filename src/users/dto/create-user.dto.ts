8e0fa1d357a26c1ad65cb73caec5bd4414760a8d
Hammed Anuoluwapo Pelumi <stationphast@gmail.com>
2025-03-13 20:08:25 +0100

    posts resources

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,36 @@
-export class CreateUserDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  MinLength,
+} from 'class-validator';
+
+export class CreateUserDto {
+  @ApiProperty({
+    example: 'user@example.com',
+    description: 'Unique email of the user',
+  })
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @ApiProperty({
+    example: 'john_doe',
+    description: 'Unique username of the user',
+    required: false,
+  })
+  @IsOptional()
+  @IsString()
+  username?: string;
+
+  @ApiProperty({
+    example: 'StrongPassword123!',
+    description: 'User password (min 6 characters)',
+  })
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(6)
+  password: string;
+}
