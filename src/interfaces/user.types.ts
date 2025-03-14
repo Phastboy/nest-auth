1e3e97d485d90282a295dee03e158fbd9691b7bd
Hammed Anuoluwapo Pelumi <stationphast@gmail.com>
2025-03-14 20:17:18 +0100

    update users logic

--- src/interfaces/user.types.ts
+++ src/interfaces/user.types.ts
@@ -1,4 +1,3 @@
 import { User } from '@prisma/client';
-import { Omit } from '@prisma/client/runtime/library';
 
 export type UserWithoutPassword = Omit<User, 'password'>;
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,16 +1,21 @@
-import { Injectable } from '@nestjs/common';
-import { CreateUserDto } from 'src/dto/create-user.dto';
-import { UpdateUserDto } from 'src/dto/update-user.dto';
-import { UserWithoutPassword } from 'src/interfaces/user.types';
-import { PrismaService } from 'src/prisma/prisma.service';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { CreateUserDto } from '../dto/create-user.dto';
+import { UpdateUserDto } from '../dto/update-user.dto';
+import { UserWithoutPassword } from '../interfaces/user.types';
+import { PrismaService } from '../prisma/prisma.service';
+import * as bcrypt from 'bcryptjs';
 
 @Injectable()
 export class UsersService {
   constructor(private readonly prismaService: PrismaService) {}
 
   async create(createUserDto: CreateUserDto): Promise<UserWithoutPassword> {
+    const hashedPassword = await bcrypt.hash(createUserDto.password, 10);
     return await this.prismaService.user.create({
-      data: createUserDto,
+      data: {
+        ...createUserDto,
+        password: hashedPassword,
+      },
       omit: {
         password: true,
       },
@@ -27,10 +32,34 @@ export class UsersService {
 
   async findOne(id: number): Promise<UserWithoutPassword> {
     return await this.prismaService.user.findUniqueOrThrow({
-      where: {
-        id,
-      },
+      where: { id },
     });
+  }
+
+  async findByEmail(email: string) {
+    return await this.prismaService.user.findUnique({
+      where: { email },
+    });
+  }
+
+  async validateUser(
+    email: string,
+    password: string,
+  ): Promise<UserWithoutPassword> {
+    const user = await this.findByEmail(email);
+    if (!user) {
+      throw new UnauthorizedException(
+        `User with email ${email} does not exist`,
+      );
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      throw new UnauthorizedException('Incorrect password');
+    }
+
+    const { password: _, ...userWithoutPassword } = user;
+    return userWithoutPassword;
   }
 
   async update(
@@ -38,17 +67,19 @@ export class UsersService {
     updateUserDto: UpdateUserDto,
   ): Promise<UserWithoutPassword> {
     return await this.prismaService.user.update({
-      where: {
-        id,
-      },
+      where: { id },
       data: updateUserDto,
+      omit: {
+        password: true,
+      },
     });
   }
 
   async remove(id: number): Promise<UserWithoutPassword> {
     return await this.prismaService.user.delete({
-      where: {
-        id,
+      where: { id },
+      omit: {
+        password: true,
       },
     });
   }
