0a9b97ad79ae1135a1ef2188c0b3676fd5de2e31
Anuoluwapo Pelumi Hammed <stationphast@gmail.com>
2025-03-22 05:45:41 +0100

    graphql module

---
+++ src/graphql/graphql.module.ts
@@ -0,0 +1,17 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'node:path';
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      typePaths: ['./**/*.graphql'],
+      definitions: {
+        path: join(process.cwd(), 'src/graphql.ts'),
+      },
+    }),
+  ],
+})
+export class GraphqlModule {}
