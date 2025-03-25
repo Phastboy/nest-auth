20011c5c2697284439a169ea8abf7951f6e54d94
Anuoluwapo Pelumi Hammed <stationphast@gmail.com>
2025-03-25 09:27:57 +0100

    register user endpoint in auth resolver

--- src/auth/auth.resolver.ts
+++ src/auth/auth.resolver.ts
@@ -3,10 +3,24 @@ import { AuthService } from './auth.service';
 import { LoginResponse } from './dto/login.response';
 import { LoginDto } from './dto/login.input';
 import { Tokens } from './dto/token.type';
+import { User } from 'src/@generated';
+import { CreateUserInput } from 'src/users/dto/create-user.input';
+import { UsersService } from 'src/users/users.service';
 
 @Resolver()
 export class AuthResolver {
-  constructor(private readonly authService: AuthService) {}
+  constructor(
+    private readonly authService: AuthService,
+    private readonly usersService: UsersService,
+  ) {}
+
+  @Mutation(() => User, {
+    name: 'register',
+    description: 'for creating a new user',
+  })
+  async register(@Args('createUserInput') createUserInput: CreateUserInput) {
+    return await this.usersService.create(createUserInput);
+  }
 
   @Mutation(() => LoginResponse, {
     name: 'login',
