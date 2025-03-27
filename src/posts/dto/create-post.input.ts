a4d1a0a0cf8d175d6b92d05954b3ba8f1e6c8eec
Anuoluwapo Pelumi Hammed <stationphast@gmail.com>
2025-03-27 16:50:00 +0100

    post management

--- src/posts/dto/create-post.input.ts
+++ src/posts/dto/create-post.input.ts
@@ -1,1 +1,13 @@
-export { PostCreateInput as CreatePostInput } from 'src/@generated';
+import { InputType, Field } from '@nestjs/graphql';
+
+@InputType()
+export class CreatePostInput {
+  @Field(() => String, { nullable: false })
+  content!: string;
+
+  @Field(() => String, { nullable: true })
+  image?: string;
+
+  @Field(() => Boolean, { nullable: true })
+  isEvent?: boolean;
+}
