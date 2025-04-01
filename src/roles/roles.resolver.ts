3ad838979af3fb7c364a2757de855e2700f7c142
Anuoluwapo Pelumi Hammed <stationphast@gmail.com>
2025-04-01 02:47:17 +0100

    role crud

--- src/roles/roles.resolver.ts
+++ src/roles/roles.resolver.ts
@@ -4,32 +4,64 @@ import { Role } from './entities/role.entity';
 import { CreateRoleInput } from './types/create-role.input';
 import { UpdateRoleInput } from './types/update-role.input';
 
+/**
+ * @class RolesResolver
+ * @description manages mutation and query used for role management
+ */
 @Resolver(() => Role)
 export class RolesResolver {
   constructor(private readonly rolesService: RolesService) {}
 
+  /**
+   * Creates a new role.
+   * @param {CreateRoleInput} createRoleInput - Input data for creating a role.
+   * @returns {Promise<Role>} The created role.
+   */
   @Mutation(() => Role)
   createRole(@Args('createRoleInput') createRoleInput: CreateRoleInput) {
-    return this.rolesService.create(createRoleInput);
+    return this.rolesService.createRole(createRoleInput);
   }
 
+  /**
+   * Retrieves all roles.
+   * @returns {Promise<Role[]>} A list of all roles.
+   */
   @Query(() => [Role], { name: 'roles' })
-  findAll() {
-    return this.rolesService.findAll();
+  async findAllRoles(): Promise<Role[]> {
+    return await this.rolesService.findAllRoles();
   }
 
+  /**
+   * Retrieves a single role by its ID.
+   * @param {number} roleId - The ID of the role to retrieve.
+   * @returns {Promise<Role>} The role with the specified ID.
+   */
   @Query(() => Role, { name: 'role' })
-  findOne(@Args('id', { type: () => Int }) id: number) {
-    return this.rolesService.findOne(id);
+  async findOneRole(@Args('roleId', { type: () => Int }) roleId: number) {
+    return this.rolesService.findOneRole(roleId);
   }
 
+  /**
+   * Updates an existing role.
+   * @param {number} roleId - The ID of the role to update.
+   * @param {UpdateRoleInput} updateRoleInput - Input data for updating the role.
+   * @returns {Promise<Role>} The updated role.
+   */
   @Mutation(() => Role)
-  updateRole(@Args('updateRoleInput') updateRoleInput: UpdateRoleInput) {
-    return this.rolesService.update(updateRoleInput.id, updateRoleInput);
+  async updateRole(
+    @Args('roleId', { type: () => Int }) roleId: number,
+    @Args('updateRoleInput') updateRoleInput: UpdateRoleInput
+  ): Promise<Role> {
+    return await this.rolesService.updateRole(roleId, updateRoleInput);
   }
 
+  /**
+   * Removes a role by its ID.
+   * @param {number} roleId - The ID of the role to remove.
+   * @returns {Promise<Role>} The removed role.
+   */
   @Mutation(() => Role)
-  removeRole(@Args('id', { type: () => Int }) id: number) {
-    return this.rolesService.remove(id);
+  async removeRole(@Args('roleId', { type: () => Int }) roleId: number) {
+    return await this.rolesService.removeRole(roleId);
   }
 }
--- src/roles/roles.service.ts
+++ src/roles/roles.service.ts
@@ -1,26 +1,145 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateRoleInput } from './types/create-role.input';
 import { UpdateRoleInput } from './types/update-role.input';
+import { PrismaService } from 'nestjs-prisma';
+import { Role } from '@prisma/client';
+import { ErrorHandler } from 'src/error-handler/error.util';
 
+/**
+ * @class RolesService
+ * @description Service for managing roles
+ */
 @Injectable()
 export class RolesService {
-  create(createRoleInput: CreateRoleInput) {
-    return 'This action adds a new role';
+  constructor(
+    private readonly prismaService: PrismaService, 
+    private readonly handler: ErrorHandler) {}
+
+  /**
+   * @method createRole
+   * @description Creates a new role
+   * @param {CreateRoleInput} createRoleInput - The input data for creating a role
+   * @returns {Role} - A message indicating the role has been created
+   */
+  async createRole(createRoleInput: CreateRoleInput): Promise<Role> {
+    try {
+    return this.prismaService.role.create({
+      data: {
+        ...createRoleInput,
+      },
+    });
+  } catch (error) {
+    this.handler.handleError(error,
+      {
+        service: 'RolesService',
+        method: 'createRole',
+        operation: 'createRole',
+        metadata: {
+          input: createRoleInput,
+        },
+      })
+    }
   }
 
-  findAll() {
-    return `This action returns all roles`;
+  /**
+   * @method findAllRoles
+   * @description Retrieves all roles
+   * @returns {Promise<Role[]>} - A promise that resolves to an array of roles
+   */
+  async findAllRoles(): Promise<Role[]> {
+    try {
+      return this.prismaService.role.findMany();
+    } catch (error) {
+      this.handler.handleError(error,
+        {
+          service: 'RolesService',
+          method: 'findAllRoles',
+          operation: 'findAllRoles',
+        })
+    }
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} role`;
+  /**
+   * @method findOneRole
+   * @description Retrieves a role by ID
+   * @param {number} id - The ID of the role to retrieve
+   * @returns {Promise<Role>} - A promise that resolves to the role
+   */
+  async findOneRole(id: number): Promise<Role> {
+    try {
+      const role = await this.prismaService.role.findUnique({
+        where: { id },
+      });
+      if (!role) {
+        throw new NotFoundException(`Role with ID ${id} not found`);
+      }
+      return role;
+    } catch (error) {
+      this.handler.handleError(error,
+        {
+          service: 'RolesService',
+          method: 'findOneRole',
+          operation: 'findOneRole',
+          metadata: {
+            id,
+          },
+        })
+    }
   }
 
-  update(id: number, updateRoleInput: UpdateRoleInput) {
-    return `This action updates a #${id} role`;
+  /**
+   * @method updateRole
+   * @description Updates a role by ID
+   * @param {number} id - The ID of the role to update
+   * @param {UpdateRoleInput} updateRoleInput - The input data for updating the role
+   * @returns {Promise<Role>} - A promise that resolves to the updated role
+   */
+  async updateRole(
+    id: number,
+    updateRoleInput: UpdateRoleInput
+  ): Promise<Role> {
+    try {
+      return this.prismaService.role.update({
+        where: { id },
+        data: {
+          ...updateRoleInput,
+        },
+      });
+    } catch (error) {
+      this.handler.handleError(error,
+        {
+          service: 'RolesService',
+          method: 'updateRole',
+          operation: 'updateRole',
+          metadata: {
+            id,
+            input: updateRoleInput,
+          },
+        })
+    }
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} role`;
+  /**
+   * @method removeRole
+   * @description Removes a role by ID
+   * @param {number} id - The ID of the role to remove
+   * @returns {string} - A message indicating the role has been removed
+   */
+  async removeRole(id: number): Promise<Role> {
+    try {
+      return this.prismaService.role.delete({
+        where: { id },
+      });
+    } catch (error) {
+      this.handler.handleError(error,
+        {
+          service: 'RolesService',
+          method: 'removeRole',
+          operation: 'removeRole',
+          metadata: {
+            id,
+          },
+        })
+    }
   }
 }
