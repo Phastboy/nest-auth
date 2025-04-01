ebcdd4b6821a78c4fbff0c8de45e57ef7ac7936a
Anuoluwapo Pelumi Hammed <stationphast@gmail.com>
2025-04-01 03:23:01 +0100

    register ErrorHandler as provider

--- src/roles/roles.module.ts
+++ src/roles/roles.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { RolesService } from './roles.service';
 import { RolesResolver } from './roles.resolver';
+import { ErrorHandler } from 'src/error-handler/error.util';
 
 @Module({
-  providers: [RolesResolver, RolesService],
+  providers: [RolesResolver, RolesService, ErrorHandler],
 })
 export class RolesModule {}
