4d68a33a079d05e3202aac42591426d76ab40bac
Anuoluwapo Pelumi Hammed <stationphast@gmail.com>
2025-04-02 11:48:20 +0100

    update

--- src/roles/roles.module.ts
+++ src/roles/roles.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { RolesService } from './roles.service';
 import { RolesResolver } from './roles.resolver';
 import { ErrorHandler } from 'src/error-handler/error.util';
+import { UsersService } from 'src/users/users.service';
 
 @Module({
-  providers: [RolesResolver, RolesService, ErrorHandler],
+  providers: [RolesResolver, RolesService, ErrorHandler, UsersService],
 })
 export class RolesModule {}
