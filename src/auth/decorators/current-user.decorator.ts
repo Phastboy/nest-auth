022c3a7a55467a613ce6e806c82eb7c51a206d6a
Anuoluwapo Pelumi Hammed <stationphast@gmail.com>
2025-04-03 16:32:47 +0100

    update current user decorator

--- src/auth/decorators/current-user.decorator.ts
+++ src/auth/decorators/current-user.decorator.ts
@@ -1,10 +1,39 @@
-import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import { createParamDecorator, ExecutionContext, NotFoundException } from '@nestjs/common';
 import { GqlExecutionContext } from '@nestjs/graphql';
 import { AuthenticatedUser } from '../types/auth.types';
+import { PrismaService } from 'nestjs-prisma';
 
+export interface ICurrentUser extends AuthenticatedUser {
+  roles?: string[];
+}
 export const CurrentUser = createParamDecorator(
-  (data: unknown, context: ExecutionContext): AuthenticatedUser => {
+  async (data: {includeRoles: boolean}, context: ExecutionContext): Promise<ICurrentUser> => {
     const ctx = GqlExecutionContext.create(context);
-    return ctx.getContext().req.user;
+    const user = ctx.getContext().req.user;
+    if (!user?.userId) {
+      throw new NotFoundException('User not found');
+    }
+
+    if (data.includeRoles) {
+      const prisma = new PrismaService();
+      const userWithRoles = await prisma.user.findUnique({
+        where: { id: user.userId },
+        select: {
+          roles: {
+            select: {
+              role: {
+                select: { name: true },
+              },
+            },
+          },
+        },
+      });
+
+      return {
+        ...user,
+        roles: userWithRoles?.roles.map((ur) => ur.role.name),
+      };
+    }
+    return user;
   },
 );
